fda5287ac5df063609c33e05135d96605b0956f2
Leonardo Sardella <lsardella@securitasargentina.com>
2025-07-03 16:24:07 -0300

    🔄 Uso de DATABASE_URL como string de conexión

--- models/db.js
+++ models/db.js
@@ -1,10 +1,8 @@
 const { Pool } = require('pg');
+
 const pool = new Pool({
-host: process.env.DB_HOST,
-user: process.env.DB_USER,
-password: process.env.DB_PASSWORD,
-database: process.env.DB_NAME,
-port: process.env.DB_PORT,
-ssl: { rejectUnauthorized: false },
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
+
 module.exports = pool;
